eb9f670ea0ee89d772c8318f40d606e77c9f7138
Vactors <aknowleskp@gmail.com>
2020-02-12 23:05:48 -0500

    Add the gamer button component

---
+++ src/components/GamerButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+class GamerButton extends React.Component{
+    render(){
+        return(
+            <Button>What's up gamers</Button>
+        )
+    }
+}
+
+export default GamerButton
