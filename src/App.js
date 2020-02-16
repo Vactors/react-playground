f12f4c5d8e6af1c5d3b253ed7211411e8fc2b29c
Vactors <aknowleskp@gmail.com>
2020-02-16 13:10:20 -0500

    replicate original shitballs page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
-import GamerButton from './components/GamerButton';
 
 function App() {
   return (
     <div className="App">
-      <GamerButton></GamerButton>
+      <h1>Welcome to shitballs.io, now with React!</h1>
     </div>
   );
 }
